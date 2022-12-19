4f9bd6dffd51cdf6b9420ecea70d146e871e14d2
Sandeepthakur2630 <115638898+Sandeepthakur2630@users.noreply.github.com>
2022-12-19 16:36:46 +0530

    fle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import JoinForm from "./JoinForm";
+import Conference from "./Conference";
+import { useEffect } from "react";
+import {
+  selectIsConnectedToRoom,
+  useHMSActions,
+  useHMSStore,
+} from "@100mslive/react-sdk";
 function App() {
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const hmsActions = useHMSActions();
+
+  useEffect(() => {
+    window.onunload = () => {
+      if (isConnected) {
+        hmsActions.leave();
+      }
+    };
+  }, [hmsActions, isConnected]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {isConnected ? <Conference /> : <JoinForm />}
     </div>
   );
 }
---
+++ src/JoinForm.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useHMSActions } from "@100mslive/react-sdk";
+
+function JoinForm() {
+  const hmsActions = useHMSActions();
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    token: "",
+  });
+
+  const handleInputChange = (e) => {
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [e.target.name]: e.target.value,
+    }));
+
+    console.log(inputValues);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await hmsActions.join({
+      userName: inputValues.name,
+      authToken: inputValues.token,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Join Room</h2>
+      <div className="input-container">
+        <input
+          required
+          value={inputValues.name}
+          onChange={handleInputChange}
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Your name"
+        />
+      </div>
+      <div className="input-container">
+        <input
+          required
+          value={inputValues.token}
+          onChange={handleInputChange}
+          id="token"
+          type="text"
+          name="token"
+          placeholder="Auth token"
+        />
+      </div>
+      <button className="btn-primary">Join</button>
+    </form>
+  );
+}
+
+export default JoinForm;
